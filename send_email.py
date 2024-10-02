9fa3034c5b7667596f761cfcee99e2ebbd0141ca
Ewel10 <80500901+Ewel10@users.noreply.github.com>
2024-10-02 18:56:03 -0300

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -7,22 +7,16 @@ def send_email():
     email_user = os.environ.get('EMAIL_USER') 
     email_password = os.environ.get('EMAIL_PASSWORD') 
     email_to = os.environ.get('EMAIL_TO') 
-
-    # Corpo do e-mail
     subject = 'Notificação do Pipeline'
     body = 'Pipeline executado com sucesso!'
-
     msg = MIMEMultipart()
     msg['From'] = email_user
     msg['To'] = email_to
     msg['Subject'] = subject
-
     msg.attach(MIMEText(body, 'plain'))
-
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(email_user, email_password)
-
     text = msg.as_string()
     server.sendmail(email_user, email_to, text)
     server.quit()
